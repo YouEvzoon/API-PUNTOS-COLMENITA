568c7a8bd7c760853347465f7d4f1af0a761bb77
YouEvzoon <evzoon1996.lo@gmail.com>
2025-11-29 11:06:29 -0600

    Update env.js

--- app/config/env.js
+++ app/config/env.js
@@ -1,12 +1,16 @@
-
-
 const env = {
-  database: 'postgres',
-  username: 'postgres',
-  password: 'Mago1996',
-  host: 'db.nfeeykysrdlcbzyxycmr.supabase.co',
+  database: 'puntos',
+  username: 'puntos_user',
+  password: '341Lg7ehXlU3HrwouLJpkeU7UT2psX32',
+  host: 'dpg-d4lia0je5dus73fqi7mg-a',   // host de Render
   port: 5432,
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
   pool: {
     max: 5,
     min: 0,
